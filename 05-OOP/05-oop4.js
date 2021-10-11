58767aaa737bd02f1f8ba595eba6ab64d4aa3107
Garnedyna Aulia Rahma <garnedyna.ar@gmail.com>
2021-10-11 21:53:30 +0700

    add 05-oop3.js

--- 05-OOP/05-oop4.js
+++ 05-OOP/05-oop4.js
@@ -1,50 +1,5 @@
-// soal no-3
-// class tersebut benama Phone
-// memiliki 3 properties yaitu:
-// brand: String
-// storage: Number
-// ram: Number
-// memiliki 4 method yaitu:
-// getBrandName [String] => akan mendapatkan isi dari field brand
-// setBrandName [void] => akan mengubah nilai dari field brand
-// printSpecification [void] => mencetak spesifikasi dari ponsel tersebut dengan format "Ponsel ini memiliki storage: $STORAGE dan ram: $RAM"
-// setSpecification [void] => akan mengubah nilai dari field storage dan ram
-// Untuk mempermudah pengerjaan kami sudah menyediakan code template
-class Phone {
-    constructor(brand, storage, ram){
-    this.brand = brand
-    this.storage = storage
-    this.ram = ram
-    }
-    getBrandName() {
-    
-        return this.brand 
-    }
-   
-    setBrandName(a) {
-       this.brand = a
-    }
-    printSpecification() {
-        console.log(`Ponsel ini memiliki storage: ${this.storage} dan ram: ${this.ram}`)
-    }
-    setSpecification(b, c){
-        this.storage = b
-        this.ram = c
-    }
- }
-
-
-const phone = new Phone("Skilvul Mobile co", 64, 4);
-
-console.log(phone.getBrandName());
-phone.setBrandName("SM.co")
-console.log(phone.getBrandName());
-
-phone.printSpecification();
-phone.setSpecification(32, 2);
-phone.printSpecification();
-
-// soal-04
+// Soal - 04
+// Kamu diminta untuk membuat beberapa class dengan spesifikasi sebagai berikut:
 // Course
 // properties:
 // subject [String] => nama pelajaran
--- 08-Asynchronous/01-asynchronous.js
+++ 08-Asynchronous/01-asynchronous.js
@@ -1,31 +1,31 @@
+// Soal - 01
+// Buatlah file 01-asynchronous.js dan copy template soal di bawah. Lalu berikan jawaban sesuai nomor soal yang disediakan.
+// Kamu diminta untuk melakukan analisa sederhana mengenai
 // 1. Apa itu synchronous?
-//Synchronous adalah proses pengeksekusian kode yang dijalankan secara berurutan berdasarkan kode yang tertulis.
-
 // 2. Apa itu asynchronous?
-//Asyncrhonous adalah proses pengeksekusian kode yang tidak sesuai dengan urutan yang ada atau bisa disebut menjalankan perintah selanjutnya tanpa menunggu perintah sebelumnya selesai.
-
 // 3. Dapatkah kita menerapkan konsep asynchronous pada browser?
-//Bisa,karena browser memerlukan waktu untuk melakukan request,sehingga agar tidak terjadi blocking pada UI, prosess asyncronus akan tetap berjalan pada background thread
-//tanpa menghalngi tampilan UI contoh penerapan nya seperti kita masih dapat melukan browsing dan juga download secara bersamaan tanpa harus menunggu proses download berakhir
-
 // 4. Apa hasil yang akan tercetak pada baris kode dibawah?
-//first log:  1
-//first log:  2
-//first log:  3
-//first log:  4
-//first log:  5
-//second log:  6
-//second log:  6
-//second log:  6
-//second log:  6
-//second log:  6
+// 5. Apabila terdapat ketidak samaan antara console.log pertama dan kedua, bisakah kalian memberikan penjelasan mengapa hal tersebut bisa
 
-// 5. Apabila terdapat ketidak samaan antara console.log pertama dan kedua, bisakah kalian memberikan penjelasan mengapa hal tersebut bisa terjadi
-//fungsi yang dideklarasikan dalam loop yang merujuk pada variabel lingkup luar dapat menyebabkan semantik yang membingungkan seharusnya menggunakan let
-
+// Answered
+// 1. Synchronous adalah proses pengeksekusian kode yang dijalankan secara berurutan.
+// 2. Asyncrhonous adalah proses pengeksekusian kode yang tidak sesuai dengan urutan dan dapat menjalankan perintah selanjutnya tanpa menunggu perintah sebelumnya selesai.
+// 3. dapat karena browser memerlukan waktu untuk melakukan request sehingga agar tidak terjadi blocking pada UI, prosess asyncronus akan tetap berjalan pada background thread.Answered
+// 4. 
+// first log:  1
+// first log:  2
+// first log:  3
+// first log:  4
+// first log:  5
+// second log:  6
+// second log:  6
+// second log:  6
+// second log:  6
+// second log:  6
+// 5. fungsi yang dideklarasikan dalam loop yang merujuk pada variabel lingkup luar yang dapat menyebabkan semantik yang membingungkan. Jadi, seharusnya menggunakan let
 // 6. Perbaiki baris kode dibawah sehingga ia akan menampilkan angka yang sama
 for (let i = 1; i<=5; i++) {
-    console.log("first log: ", i); // 01 - Pertama
-    setTimeout(() => console.log("second log: ", i), 100); // 02 - Kedua
-  }
+  console.log("first log: ", i); // 01 - Pertama
+  setTimeout(() => console.log("second log: ", i), 100); // 02 - Kedua
+}
 
